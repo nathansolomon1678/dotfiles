1c3256af12f6af133f14ec52d2ca589cfdc82d84
Nathan Solomon <nathansolomon1678@gmail.com>
2018-02-07 20:16:05 -0800

    Add init.vim

--- example.plugin.zsh
+++ example.plugin.zsh
@@ -57,3 +57,7 @@ alias fun='command cat /dev/urandom'
 alias format='clang-format -i'
 alias git praise='git blame'
 alias hack='hollywood'
+
+#TODO(Nathan) create vim fold to hide the following aliases
+alias ccd='command cd'
+alias cd='xdg-open && ccd' #TODO(Nathan) url for rick-roll prank
